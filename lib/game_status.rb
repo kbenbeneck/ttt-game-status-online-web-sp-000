4e71f9dac0a431964fe933a4399de7b0412b7b8c
Karl Benbeneck <kbenbeneck@gmail.com>
2018-10-09 02:22:53 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,9 +25,5 @@ def won?(board)
     position_3 = board[win_index_3]
     if ((position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O"))
       return win_combination
-    else
-      # do nothing
-    end
   end
-  return nil
 end
